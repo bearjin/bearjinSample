c08d596ad2d3fd7c494077493a3b2ecc002c1f0e
bearjin <kwj5030@gmail.com>
2021-10-06 21:25:53 +0900

    Update: 탭메뉴 콘텐츠 추가

--- tabMenu/index.js
+++ tabMenu/index.js
@@ -5,15 +5,23 @@ const tabHandler = () => {
     item.addEventListener('click', (e) => {
       const target = e.target;
       const buttons = item.querySelectorAll('.tab-wrap__button');
+      const contents = document.querySelectorAll('.tab-contents__item');
+      const tabNum = target.dataset.tabnum;
 
       buttons.forEach(button => {
         button.classList.remove('tab-wrap__button--active');
       });
 
+      contents.forEach(content => {
+        content.classList.remove('tab-contents__item--active');
+      });
+
       if (target.classList.contains('tab-wrap__button--active')) {
         target.target.classList.remove('tab-wrap__button--active');
+        contents[tabNum].classList.remove('tab-contents__item--active');
       } else {
         target.classList.add('tab-wrap__button--active');
+        contents[tabNum].classList.add('tab-contents__item--active');
       }
     });
   });
