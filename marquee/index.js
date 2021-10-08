e51616e68391e1316a73b5acf5ab3205036be519
bearjin <kwj5030@gmail.com>
2021-10-08 14:09:35 +0900

    Update: 모션 스크립트 추가

--- marquee/index.js
+++ marquee/index.js
@@ -1,17 +1,19 @@
 function moveRightToLeft() {
-  $textLi.find(".text_inner").find(".top").stop().css({ left: 0 });
-  $textLi.find(".text_inner").find(".top").animate({
-    left: '-=' + textTopW,
-  }, 25000, 'linear', function () {
-    textAniTop();
-  });
-}
-
-function moveLeftToRight() {
-  $textLi.find(".text_inner").find(".bottom").stop().css({ right: 0 });
-  $textLi.find(".text_inner").find(".bottom").animate({
-    right: '-=' + textBtmW,
-  }, 25000, 'linear', function () {
-    textAniBtm();
-  });
+  const wrap = document.querySelector('.marquee--script');
+  const items = wrap.querySelectorAll('.marquee__item');
+  const wrapSize = wrap.getBoundingClientRect();
+  let left = 0;
+  let interVal;
+  const frame = () => {
+    left++;
+    items.forEach(item => {
+      item.style.transform = `translateX(-${left}px)`;
+    });
+    if (left > wrapSize.width) {
+      clearInterval(interVal);
+      left = 0;
+      interVal = setInterval(frame, 10);
+    }
+  }
+  interVal = setInterval(frame, 10);
 }
