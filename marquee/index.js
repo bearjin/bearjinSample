f022bb609a18b03a3307558f5af2fa4c3580366d
bearjin <kwj5030@gmail.com>
2021-10-08 16:09:55 +0900

    Update: 스크립트 수정

--- marquee/index.js
+++ marquee/index.js
@@ -1,19 +1,34 @@
-function moveRightToLeft() {
-  const wrap = document.querySelector('.marquee--script');
-  const items = wrap.querySelectorAll('.marquee__item');
-  const wrapSize = wrap.getBoundingClientRect();
-  let left = 0;
-  let interVal;
-  const frame = () => {
-    left++;
-    items.forEach(item => {
-      item.style.transform = `translateX(-${left}px)`;
-    });
-    if (left > wrapSize.width) {
-      clearInterval(interVal);
-      left = 0;
-      interVal = setInterval(frame, 10);
+const marqueeMotion = () => {
+  const marquees = document.querySelectorAll('.marquee');
+  marquees.forEach(marquee => {
+    const direction = marquee.dataset.direction;
+    let marqueeWidth = marquee.getBoundingClientRect().width;
+    const marqueeItems = marquee.querySelectorAll('.marquee__item');
+    const marqueeItemWidth = marqueeItems[0].getBoundingClientRect().width;
+    let move = 0;
+    let moveInterVal;
+
+    if (marqueeItems.length === 1) {
+      marquee.classList.add('marquee--itemOne');
+      marqueeWidth += marqueeItemWidth;
+      if (direction === 'right') marqueeItems[0].style.marginLeft = `-${marqueeItemWidth}px`;
     }
-  }
-  interVal = setInterval(frame, 10);
+    if (marqueeItems.length === 2) {
+      marquee.classList.add('marquee--itemTwo');
+    }
+
+    const frame = () => {
+      move++;
+      marqueeItems.forEach(item => {
+        item.style.transform = direction === 'left' ? `translateX(-${move}px)` : `translateX(${move}px)`;
+      });
+
+      if (move > marqueeWidth) {
+        clearInterval(moveInterVal);
+        move = 0;
+        moveInterVal = setInterval(frame, 10);
+      }
+    }
+    moveInterVal = setInterval(frame, 10);
+  });
 }
