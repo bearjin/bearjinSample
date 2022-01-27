012c7a683abdb936e304fc1ee4413c970968add3
bearjin <kwj5030@gmail.com>
2022-01-27 18:30:03 +0900

    Update: 헤더, 메뉴바

--- global/src/App.js
+++ global/src/App.js
@@ -1,11 +1,15 @@
-import ProductList from './components/ProductList';
 import GlobalStyles from './style/GlobalStyles';
+import Header from './components/Header';
+import ProductList from './components/ProductList';
+import MenubarBottom from './components/MenubarBottom';
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
+      <Header />
       <ProductList />
+      <MenubarBottom />
     </>
   );
 };
---
+++ global/src/style/mixins.js
@@ -0,0 +1,13 @@
+import { css } from 'styled-components';
+
+const flexCenter = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const mixins = {
+  flexCenter,
+}
+
+export default mixins;
